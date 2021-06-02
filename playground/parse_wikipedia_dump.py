b96fbeaab640d736ac46b40ac8f8c722bfa5865a
Dani Fdez <danifdezalvarez@gmail.com>
2021-06-02 20:52:07 +0200

    Working on #5

---
+++ playground/parse_wikipedia_dump.py
@@ -0,0 +1,15 @@
+
+from wikipedia_shexer.utils.dump_wikipedia_utils import DumpWikipediaUtils
+from wikipedia_shexer.io.xml.wikipedia_dump_yielder import WikimediaDumpYielder
+
+
+source_file = r"F:\datasets\enwiki-20210501-pages-articles-multistream.xml\enwiki-20210501-pages-articles-multistream.xml"
+
+yielder = WikimediaDumpYielder(source_file=source_file)
+
+for a_node in yielder.yield_xml_nodes(limit=20):
+    model = DumpWikipediaUtils.extract_model_abstract(xml_node=a_node)
+
+
+
+print("Done!")
