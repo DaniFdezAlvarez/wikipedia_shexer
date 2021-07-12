d7fe07c814388398b4df8776b023edaf9d1c8885
Dani Fdez <danifdezalvarez@gmail.com>
2021-07-12 20:21:47 +0200

    test dockerfile_script reading from ini

--- docker/extract_features/script.py
+++ docker/extract_features/script.py
@@ -1,1 +1,26 @@
-print("Hola mundo")
+from configparser import ConfigParser
+import sys
+
+def run(ini_file):
+    parser = ConfigParser(ini_file)
+
+
+    print("Ontology file: {}".format(parser.get("ontology", "ontology_path")))
+    print("Wikipedia dump file: {}".format(parser.get("wikipedia", "dump_path")))
+    print("Important nodes file: {}".format(parser.get("targets", "nodes_path")))
+
+    print("Dbepdia")
+    print("Types: {}".format(parser.get("dbpedia", "types_path")))
+    print("Wikilinks: {}".format(parser.get("dbpedia", "wikilinks_path")))
+
+    dbp_dump_files = parser.get("dbpedia", "dump_path").split("|")
+    print("Dump_files: {}".format(", ".join(dbp_dump_files)))
+
+
+    print("Results will be outputed to: {}".format(parser.get("targets", "result_path")))
+
+
+if __name__ == "__main__":
+    run(sys.argv[0])
+
+
