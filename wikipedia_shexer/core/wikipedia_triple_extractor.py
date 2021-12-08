a142d2fead7f746bc089eea9d22d5538d251c517
Dani Fdez <danifdezalvarez@gmail.com>
2021-12-08 13:00:31 +0100

    Working on #35

--- wikipedia_shexer/core/wikipedia_triple_extractor.py
+++ wikipedia_shexer/core/wikipedia_triple_extractor.py
@@ -7,11 +7,12 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from wikipedia_shexer.utils.wikipedia_dbpedia_conversion import page_id_to_DBpedia_id
-from wikipedia_shexer.const import NAME_COLS, FEATURE_COLS, COL_DIRECT, COL_INSTANCE, COL_MENTION, COL_PROP
+from wikipedia_shexer.const import NAME_COLS, FEATURE_COLS, COL_DIRECT, COL_INSTANCE, \
+    COL_MENTION, COL_PROP, COL_POSITIVE, COL_BACK_LINK
 
 _TRIPLE_PATTERN = "<{}> <{}> <{}> .\n"
 
-MIN_SAMPLE = 20
+MIN_SAMPLE = 30
 
 
 class WikipediaTripleExtractor(object):
@@ -37,22 +38,34 @@ class WikipediaTripleExtractor(object):
                                        training_data_file,
                                        callback,
                                        inverse=True):
-        self._load_internal_strcutures()
+        self._load_internal_structures()
         self._f_extractor.rows_to_file_from_page_list(page_list=self._target_instances(titles_file),
                                                       inverse=inverse,
                                                       file_path=rows_out_file)
         self._load_classifiers(training_data_file=training_data_file,
                                callback=callback)
         self._write_predicted_triples(triples_out_file=triples_out_file,
-                                      rows_out_file=rows_out_file)
+                                      rows_source_file=rows_out_file)
 
-    def _write_predicted_triples(self, triples_out_file, rows_out_file):
+    def extract_triples_of_rows(self,
+                                rows_file,
+                                triples_out_file,
+                                training_data_file,
+                                callback):
+        self._load_classifiers(training_data_file=training_data_file,
+                               callback=callback)
+        self._write_predicted_triples(triples_out_file=triples_out_file,
+                                      rows_source_file=rows_file)
+
+    def _write_predicted_triples(self, triples_out_file, rows_source_file):
         with open(triples_out_file, "w") as out_str:
+            print("Voy eh")
             for prop_key in self._clf_battery:
+                print(prop_key)
                 self._write_triples_for_a_prop_model(
                     out_stream=out_str,
                     prop_key=prop_key,
-                    rows_out_file=rows_out_file)
+                    rows_out_file=rows_source_file)
 
     def _write_triples_for_a_prop_model(self, prop_key, out_stream, rows_out_file):
         target_data = self._load_prop_target_data(prop_key=prop_key,
@@ -66,10 +79,12 @@ class WikipediaTripleExtractor(object):
                                    out_stream=out_stream)
 
     def _write_actual_triples(self, target_data, y_results, out_stream):
-        for index, row in target_data.iterrows():
+        index = 0
+        for row in target_data.iterrows():
             if y_results[index] == 1:
                 self._write_triple(out_stream=out_stream,
                                    triple=self._build_triple_from_row(row))
+            index += 1
 
     def _write_triple(self, out_stream, triple):
         out_stream.write(_TRIPLE_PATTERN.format(triple[0],
@@ -95,10 +110,10 @@ class WikipediaTripleExtractor(object):
             self._read_target_data(rows_out_file)
         return self._target_data[self._target_data[COL_PROP] == prop_key]
 
-    def _read_target_data(self, rows_out_file):
-        self._target_data = pd.read_csv(rows_out_file, header=None, names=NAME_COLS, sep=";")
+    def _read_target_data(self, target_file):
+        self._target_data = self._read_pandas_csv(target_file=target_file)
 
-    def _load_internal_strcutures(self):
+    def _load_internal_structures(self):
         ontology = Ontology(source_file=self._ontology_file)
         typing_cache = TypingCache(source_file=self._typing_file,
                                    ontology=ontology,
@@ -118,15 +133,39 @@ class WikipediaTripleExtractor(object):
         return uris_yielder.list_lines()
 
     def _load_classifiers(self, training_data_file, callback):
-        features = pd.read_csv(training_data_file, header=None, names=NAME_COLS, sep=";")
+        features = self._read_pandas_csv(target_file=training_data_file)
         for a_prop in set(features[COL_PROP]):
             prop_features = features[features[COL_PROP] == a_prop]
-            if len(features > MIN_SAMPLE):
-                pass
-            X = prop_features[FEATURE_COLS]
-            y = prop_features.positive.astype('int')
-            if len(np.unique(y)) <= 1:
-                return 1.0
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
-            a_clasiff = callback().fit(X_train, y_train)
-            self._clf_battery[a_prop] = a_clasiff
+            if len(prop_features) > MIN_SAMPLE:
+                X = prop_features[FEATURE_COLS]
+                y = prop_features.positive.astype('int')
+                values = np.unique(y)
+                if len(values) == 1:
+                    self._clf_battery[a_prop] = self._dumb_classifier(value=values[0])
+                else:
+                    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+                    a_clasiff = callback().fit(X_train, y_train)
+                    self._clf_battery[a_prop] = a_clasiff
+
+    def _dumb_classifier(self):
+        pass
+
+    def _read_pandas_csv(self, target_file):
+        features = pd.read_csv(target_file, header=None, names=NAME_COLS, sep=";")
+        self._map_bool_to_integer(dataframe=features, target_cols=[COL_POSITIVE, COL_BACK_LINK, COL_DIRECT])
+        return features
+
+    @staticmethod
+    def _map_bool_to_integer(dataframe, target_cols):
+        for i in range(len(dataframe)):
+            for col_name in target_cols:
+                dataframe.at[i, col_name] = 1 if dataframe.at[i, col_name] == 'True' else 0
+
+
+class DumpClassifier(object):
+
+    def __init__(self, value):
+        self._value = value
+
+    def predict(self, dataframe):
+        pass  # TODO ARRAY OF SIZE LEN(DATAFRAME) FULL OF $VALUE$s
